d00282c61145116c2b352483e145f1db710e7b37
Kirby <kirbyc6@gmail.com>
2020-09-17 16:37:29 -0400

    working on heroku

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ const PORT = 1337
 db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
   .then(() => {
     console.log('db synced')
-    app.listen( process.env.port ||
+    app.listen( process.env.PORT ||
       PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
   })
