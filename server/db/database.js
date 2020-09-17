60906645d6bdbcc69f34c40c23601a0062ba415f
Kirby <kirbyc6@gmail.com>
2020-09-17 16:28:19 -0400

    deploy heroky

--- server/db/database.js
+++ server/db/database.js
@@ -14,8 +14,18 @@ const pkg = require('../../package.json');
 const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}_test` : pkg.name;
 console.log(chalk.yellow(`Opening database connection to ${dbName}`));
 
-const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
-  logging: false,
-});
+const db = new Sequelize(
+  `postgres://ghreubtdgaslnd:4ac35447228db5cebd3d5d0b053ea12f4b1b4c8f25a8c67ff75f4379401ea4f8@ec2-54-160-202-3.compute-1.amazonaws.com:5432/d3epcubi8jtdkj`,
+  {
+    logging: false,
+    dialectOption: {
+      ssl: true,
+    }
+  }
+)
+
+// const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
+//   logging: false,
+// });
 
 module.exports = db;
