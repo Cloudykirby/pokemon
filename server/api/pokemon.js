fe0cdf6d5e1542090ce4797901b70ab3060ddf68
Kirby <kirbyc6@gmail.com>
2020-09-18 12:34:37 -0400

    added in lodash

--- server/api/pokemon.js
+++ server/api/pokemon.js
@@ -1,14 +1,16 @@
 const router = require('express').Router()
 const {Op} = require('sequelize')
 const { Pokemon } = require('../db')
+const lodash = require('lodash')
 
 
 // /api/pokemon/random
 router.get('/random', async (req, res, next) =>{
   try {
+
     let randomArray = []
     while(randomArray.length !== 3){
-      let num = Math.floor(Math.random() * 151) + 1
+      let num = lodash.random.bind(0, 151, true)
       if (!randomArray.includes(num)){
         randomArray.push(num)
       }
